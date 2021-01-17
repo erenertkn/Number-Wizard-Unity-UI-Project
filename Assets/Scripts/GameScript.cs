8163975812b9602de67bdd2c84f9d9bd7b834824
erenertkn <erenertkn@users.noreply.github.com>
2021-01-17 12:42:37 +0300

    First commit
    My first Unity project

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class GameScript : MonoBehaviour
+{
+    private int Guess_num,min,a,max;
+    System.Random rand = new System.Random();
+    private int rand_num;
+    void Start()
+    {
+        rand_num = rand.Next(1,1000);
+        Guess_num = rand_num;
+        a = 0;
+        GameObject.Find("txtGuess").GetComponent<Text>().text = rand_num.ToString();
+        min = 1;
+        max = 1000;
+
+    }
+    public void PressUp()
+    {
+        min = Guess_num;
+        Guess_num = (Guess_num + max) / 2;
+        
+    }
+    public void PressDown()
+    {
+        Guess_num = (Guess_num - min) / 2;
+        max = Guess_num;
+
+    }
+    
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    private int currentSceneIndex;
+    private Text myIF;
+
+    public void SceneChanger()
+    {
+
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 2)                    //Başa sarma taktiğim. Videoda yeni bir method oluşturup son ekrandaki
+        {                                             //butona o methodu tanımlıyordu.
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+    }
+
+    public void FirstButton()
+    {
+        myIF = GameObject.Find("InputField").GetComponentInChildren<Text>();
+        PlayerPrefs.SetInt("number", Convert.ToInt32(myIF));
+        SceneManager.LoadScene(1);
+    }
+
+
+}
